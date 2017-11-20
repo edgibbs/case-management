5c4995fa51a0723ea65231ee26715e705c6572dc
Ed Gibbs <edward_gibbs@yahoo.com>
2017-11-20 14:51:00 -0800

    Initial codeclimate config (#65)
    * Initial codeclimate config
    
    * Attempts to DRY up eslintrc files
    
    * Cleansup codeclimate config
    
    * Add enzyme and refactor hello world unit test (#63)

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-feature "index page" do
-  scenario "displays default rails" do
-    visit "/"
+feature 'index page' do
+  scenario 'displays default rails' do
+    visit '/'
     expect(page).to have_content('Hello')
   end
 end
