edc26264840bf1b248565c5e2070151abf9e2fb6
Melbourne <30814094+mlipardo@users.noreply.github.com>
2017-12-07 10:22:11 -0800

    Clean landing page  case 899 (#81)
    * Clean Landing page.
    
    * Clean Landing page.
    
    * Change page header title.

--- app/javascript/landingPage/landingPage.js
+++ app/javascript/landingPage/landingPage.js
@@ -4,16 +4,22 @@ import { Cards, GlobalHeader, PageHeader } from 'react-wood-duck';
 export default class LandingPage extends React.Component {
   render() {
     return (
-      <div id="landingPage">
+      <div>
         <GlobalHeader />
-        <PageHeader pageTitle="Dashboard" />
-        <Cards cardHeaderText="Caseload" />
-        <Cards cardHeaderText="Client" />
-        <Cards cardHeaderText="Quick Links">
-          <div style={{ paddingTop: '1.5rem' }}>
-            <a href="/clients/show"> Link To Client ID Page </a>
+        <PageHeader pageTitle="Dashboard" button={null} />
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <Cards cardHeaderText="Cases" />
+              <Cards cardHeaderText="Referrals" />
+              <Cards cardHeaderText="Quick Links">
+                <div style={{ paddingTop: '1.5rem' }}>
+                  <a href="/clients/show"> Link To Client ID Page </a>
+                </div>
+              </Cards>
+            </div>
           </div>
-        </Cards>
+        </div>
       </div>
     );
   }
