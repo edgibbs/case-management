0d98038d018e6c3eeb2cfe7828690c5a1baff6e4
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2017-12-07 16:41:55 -0800

    Fix file watching in docker (#83)

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,10 @@
-const environment = require('./environment')
+const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig()
+const devConfig = environment.toWebpackConfig();
+devConfig.devServer.watchOptions = {
+  ignored: /node_modules/,
+  aggregateTimeout: 300,
+  poll: 500,
+};
+
+module.exports = devConfig;
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+docker build -t cwds/casemanagement .
 docker run \
   -it \
   --rm  \
   --publish "3000":"3000" \
+  --publish "3035":"3035" \
   --mount type=bind,src="$(pwd)"/app,dst=/app/app,consistency=cached \
-  cwds/casemanagement foreman start -f Procfile.dev
+  cwds/casemanagement foreman start -f Procfile.docker.dev
