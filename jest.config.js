7cf4bb8e899b4e91ab553be3ac8e969603e038c7
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2017-12-13 05:19:52 -0800

    Move jest coverage output (#92)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
+const path = require('path');
 module.exports = {
   verbose: true,
   rootDir: './app/javascript',
   setupTestFrameworkScriptFile: '<rootDir>/testing/setupTests.js',
+  coverageDirectory: path.resolve(__dirname, 'coverage', 'javascript'),
 };
