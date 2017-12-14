42e8f9b3b62d7ff0c15f79f114cd72452522ac89
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2017-12-13 20:31:06 -0800

    Fix develop in docker set up (#91)
    * Revert docker workaround, use webpacker for config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,10 +1,3 @@
-const environment = require('./environment');
+const environment = require('./environment')
 
-const devConfig = environment.toWebpackConfig();
-devConfig.devServer.watchOptions = {
-  ignored: /node_modules/,
-  aggregateTimeout: 300,
-  poll: 500,
-};
-
-module.exports = devConfig;
+module.exports = environment.toWebpackConfig()
