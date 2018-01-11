3c981f4d90424e3d24218e33993be78a0c28c6c2
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2018-01-11 04:33:50 -0800

    Refactor to silence CodeClimate whines (#141)

--- scripts/stylelint-formatter.tap.js
+++ scripts/stylelint-formatter.tap.js
@@ -1,10 +1,3 @@
-function outputDiagnostics(diagnostic) {
-  var prefix = '  ';
-  var output = prefix + '---\n';
-  output += prefix + diagnostic.split('\n').join('\n' + prefix);
-  output += '...\n';
-  return output;
-}
 function formatter(results) {
   var output = 'TAP version 13\n1..' + results.length + '\n';
   results.forEach(function(result, id) {
@@ -14,15 +7,7 @@ function formatter(results) {
     if (messages.length > 0) {
       testResult = 'not ok';
       messages.forEach(function(message) {
-        var diagnostic = {
-          message: message.text,
-          severity: message.severity,
-          data: {
-            line: message.line || 0,
-            column: message.column || 0,
-            ruleId: message.rule || '',
-          },
-        };
+        var diagnostic = toDiagnostic(message);
         if ('message' in diagnostics) {
           if (typeof diagnostics['messages'] === 'undefined') {
             diagnostics['messages'] = [];
@@ -34,10 +19,29 @@ function formatter(results) {
       });
     }
     output += testResult + ' ' + (id + 1) + ' - ' + result.source + '\n';
+    // If we have an error include diagnostics
     if (messages.length > 0) {
       output += outputDiagnostics(diagnostics);
     }
   });
   return output;
+}
+function outputDiagnostics(diagnostic) {
+  var prefix = '  ';
+  var output = prefix + '---\n';
+  output += prefix + diagnostic.split('\n').join('\n' + prefix);
+  output += '...\n';
+  return output;
+}
+function toDiagnostic(message) {
+  return {
+    message: message.text,
+    severity: message.severity,
+    data: {
+      line: message.line || 0,
+      column: message.column || 0,
+      ruleId: message.rule || '',
+    },
+  };
 }
 module.exports = formatter;
